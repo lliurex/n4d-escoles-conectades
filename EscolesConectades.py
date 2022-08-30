e5a1a135a5cb0d8461cdffaaedb462315abb6d7d
Lt-Henry <quiqueiii@gmail.com>
2022-08-30 12:28:15 +0200

    Doing a scan of access points

--- EscolesConectades.py
+++ EscolesConectades.py
@@ -1,9 +1,29 @@
 import n4d.responses
 
+import NetworkManager as nm
+
+
 class EscolesConectades:
+
+	ERROR_NO_WIFI_DEV = -1
+
 	def __init__(self):
 		pass
 
 
 	def scan_network(self):
-		return n4d.responses.build_successful_call_response()
+		wifi = None
+		for device in nm.Device.all():
+			if (device.DeviceType == nm.NM_DEVICE_TYPE_WIFI):
+				wifi = device
+				break
+
+		if (not wifi):
+			return n4d.responses.build_failed_call_response(EscolesConectades.ERROR_NO_WIFI_DEV,"No wireless device available")
+
+		wifi.RequestScan([])
+		aps = []
+		for ap in wifi.AccessPoints:
+			aps.append([ap.Ssid,ap.Strength])
+
+		return n4d.responses.build_successful_call_response(aps)
